9607a31b821b4682b3c19bd26b1c2f33f5f261d5
Basil Atawneh <basil.atawneh.1998@gmail.com>
2022-03-24 19:57:55 +0200

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
 const App = () => {
   const [expenses, setExpenses] = useState([]);
+  const API = 'https://expenses-project-2e3d1-default-rtdb.firebaseio.com/expences.json';
+  
+  const getData = () => {
+    fetch(API)
+      .then(res => res.json())
+      .then(res => {
+        const finalExpenses = [];
+        for (let expens in res) {
+          finalExpenses.push({
+            ...res[expens],
+            date: new Date(res[expens].date)
+          });
+        }
+
+        setExpenses(finalExpenses)
+      })
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const addExpenseHandler = (expense) => {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(expense)
+    }
+    fetch(API, options)
+    .then(res => res.json())
+    .then(res=>{
+      getData();
+    })
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
